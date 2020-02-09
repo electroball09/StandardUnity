e9edae7bcb922fcbd7c50ab9bcb7f57ac572a5b9
electro <edgrbrmn@gmail.com>
2020-02-08 19:21:27 -0700

    add build processor

---
+++ code/Editor/BuildProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using System.IO;
+
+public class BuildProcessor : IPostprocessBuildWithReport
+{
+    public int callbackOrder => 0;
+
+    public void OnPostprocessBuild(BuildReport report)
+    {
+        if (report.summary.result != BuildResult.Succeeded) return;
+
+        string outputPath = report.summary.outputPath + "/";
+        string sourcePath = Directory.GetCurrentDirectory() + "/";
+
+        Debug.LogFormat("Copying Config file from {0} to {1}", sourcePath, outputPath);
+    }
+}
