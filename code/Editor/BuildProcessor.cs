b531c6a8d08ca7b8a7570c2ff46ba85c6be9c938
electro <edgrbrmn@gmail.com>
2020-04-22 23:36:47 -0600

    no longer ignores unity meta files

--- code/Editor/BuildProcessor.cs
+++ code/Editor/BuildProcessor.cs
@@ -8,15 +8,17 @@ using System.IO;
 
 public class BuildProcessor : IPostprocessBuildWithReport
 {
-    public int callbackOrder => 0;
+    public int callbackOrder => 1;
 
     public void OnPostprocessBuild(BuildReport report)
     {
-        if (report.summary.result != BuildResult.Succeeded) return;
+        //if (report.summary.result != BuildResult.Succeeded) return;
 
-        string outputPath = report.summary.outputPath + "/";
-        string sourcePath = Directory.GetCurrentDirectory() + "/";
+        string outputPath = new FileInfo(report.summary.outputPath).Directory.FullName + "/Config/";
+        string sourcePath = Directory.GetCurrentDirectory() + "/Config/";
 
         Debug.LogFormat("Copying Config file from {0} to {1}", sourcePath, outputPath);
+
+        FileUtil.CopyFileOrDirectory(sourcePath, outputPath);
     }
 }
---
+++ code/SingletonInstantiator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonInstantiator : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod]
+    static void LoadConsole()
+    {
+        GameObject console = Resources.Load("IngameDebugConsole") as GameObject;
+
+        Instantiate(console);
+        DontDestroyOnLoad(console);
+    }
+}
