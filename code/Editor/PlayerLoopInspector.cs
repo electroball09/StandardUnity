2aabaf753f478ce2d60a2e7f5e36f32cc87e7330
electro <edgrbrmn@gmail.com>
2020-09-09 19:40:02 -0600

    added basics of rosace

---
+++ code/Editor/PlayerLoopInspector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.LowLevel;
+using UnityEditor;
+
+public class PlayerLoopInspector : EditorWindow
+{
+    static Vector2 scrollPos = Vector2.zero;
+
+    [MenuItem("Tools/Player Loop Inspector")]
+    static void ShowWindow()
+    {
+        GetWindow<PlayerLoopInspector>();
+    }
+
+    static void DrawSystem(ref PlayerLoopSystem loop)
+    {
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.LabelField(loop.type != null ? loop.type.ToString() : "null" );
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.Space(8);
+        EditorGUILayout.BeginVertical();
+        if (loop.subSystemList != null)
+            for (int i = 0; i < loop.subSystemList.Length; i++)
+                DrawSystem(ref loop.subSystemList[i]);
+        EditorGUILayout.EndVertical();
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.EndVertical();
+    }
+
+    void OnGUI()
+    {
+        PlayerLoopSystem loop = PlayerLoop.GetCurrentPlayerLoop();
+
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+        DrawSystem(ref loop);
+        EditorGUILayout.EndScrollView();
+    }
+}
---
+++ code/Rosace/Rosace.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.LowLevel;
+using UnityEngine.PlayerLoop;
+
+public class Rosace
+{
+    static PlayerLoopSystem playerLoop;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+    static void InitRosace()
+    {
+        Debug.Log("ROSACE INIT");
+
+        playerLoop = PlayerLoop.GetDefaultPlayerLoop();
+
+        PlayerLoopSystem[] newSystems = new PlayerLoopSystem[playerLoop.subSystemList.Length + 1];
+        Array.Copy(playerLoop.subSystemList, newSystems, playerLoop.subSystemList.Length);
+
+        PlayerLoopSystem rosaceSystem = new PlayerLoopSystem()
+        {
+            subSystemList = new PlayerLoopSystem[1]
+            {
+                new PlayerLoopSystem()
+                {
+                    type = typeof(RosaceUpdate),
+                    updateDelegate = RosaceUpdate.Update
+                }
+            },
+            type = typeof(Rosace)
+        };
+
+        for (int i = 0; i < newSystems.Length; i++)
+        {
+            if (newSystems[i].type == typeof(Update))
+            {
+                for (int j = newSystems.Length - 1; j > i; j--)
+                {
+                    newSystems[j] = newSystems[j - 1];
+                }
+
+                newSystems[i] = rosaceSystem;
+
+                break;
+            }
+        }
+
+        playerLoop.subSystemList = newSystems;
+
+        PlayerLoop.SetPlayerLoop(playerLoop);
+
+        Debug.Log("ROSACE INJECTED");
+    }
+
+    struct RosaceUpdate
+    {
+        public static void Update()
+        {
+            Debug.Log("rosace update!");
+        }
+    }
+}
