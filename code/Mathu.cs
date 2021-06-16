6c61dcd163c6b2b0382b4e1dc332e1a63fc0a199
electroball09 <edgrbrmn@gmail.com>
2021-06-15 23:35:29 -0600

    added mathu

---
+++ code/Mathu.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public struct Mathu
+{
+    public const float FtLbToNM = 1.36f;
+    public const float NMToFtLb = 0.74f;
+}
